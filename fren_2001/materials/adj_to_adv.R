0258270e44aec4bc6647a063437fbb7b6552d86b
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2024-02-24 09:54:38 -0500

    Word generator for adv game

---
+++ fren_2001/materials/adj_to_adv.R
@@ -0,0 +1,8 @@
+# For the adjectives to adverbs games
+adjectives <- c("courageux", "heureux", "bon", "indépendant", "vrai", "sec",
+                "rapide", "mauvais", "aveugle", "obstiné", "bruyant", "efficace",
+                "élégant", "nouveau", "public", "tranquille", "énorme", "éperdu",
+                "puissant", "seul", "naturel", "profond", "décent", "fort",
+                "précis", "lent", "objectif", "faux", "actif", "long", "frais",
+                "merveilleux", "poli", "immédiat", "fréquent", "constant")
+sample(adjectives, length(adjectives))
